8b422f69e9f60ef4f040fec7303bfc09ea160e41
Robert Czarnik <robert.czarnik98@gmail.com>
2019-11-28 18:40:51 +0100

    get all records from database

---
+++ CurrencyConverter/src/main/java/pl/robertczarnik/currencyconverter/calls/CallController.java
@@ -0,0 +1,23 @@
+package pl.robertczarnik.currencyconverter.calls;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/calls")
+@RestController
+class CallController {
+
+    private CallRepository callRepository;
+
+    public CallController(CallRepository callRepository) {
+        this.callRepository = callRepository;
+    }
+
+    @GetMapping("")
+    List<Call> getActualCurrencyRate(){
+        return callRepository.findAll();
+    }
+}
